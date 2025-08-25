34c69f9fc2669892b130933db76bdce4c1366354
Romanenko Yliia(mac) <romany0507306677@gmail.com>
2025-08-25 18:06:13 +0300

    modif path

--- src/shared/config/routes/apiRoutes.js
+++ src/shared/config/routes/apiRoutes.js
@@ -1,13 +1,13 @@
 export const apiRoutes = {
   auth: {
-    login: "/auth/login",
-    logout: "/auth/logout",
-    refresh: "/auth/refresh",
-    profile: "/auth/profile",
+    login: "auth/login",
+    logout: "auth/logout",
+    refresh: "auth/refresh",
+    profile: "auth/profile",
   },
-  users: "/users",
-  posts: "/posts",
-  comments: "/comments",
-  getCommentsByPostId: "/comments",
+  users: "users",
+  posts: "posts",
+  comments: "comments",
+  getCommentsByPostId: "comments",
   //   getCommentsByPostId: (postId) => `/comments?postId=${postId}`,
 };
